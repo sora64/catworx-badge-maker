98c4707139a5c680f02cad4b2301c43784f32a58
sora64 <phelpsa64@gmail.com>
2022-10-28 18:48:41 -0400

    moved GetEmployees method to its own file

---
+++ PeopleFetcher.cs
@@ -0,0 +1,32 @@
+namespace CatWorx.BadgeMaker
+{
+    class PeopleFetcher
+    {
+        public static List<Employee> GetEmployees()
+        {
+            List<Employee> employees = new List<Employee>();
+            // Collect user values until the value is an empty string
+            while (true)
+            {
+                Console.WriteLine("Please enter a  first name (leave empty to exit): ");
+                string firstName = Console.ReadLine() ?? "";
+                // Break if the user hits ENTER without typing a name
+                if (firstName == "")
+                {
+                    break;
+                }
+
+                Console.Write("Enter a last name: ");
+                string lastName = Console.ReadLine() ?? "";
+                Console.Write("Enter ID: ");
+                int id = Int32.Parse(Console.ReadLine() ?? "");
+                Console.Write("Enter a photo URL: ");
+                string photoUrl = Console.ReadLine() ?? "";
+                Employee currentEmployee = new Employee(firstName, lastName, id, photoUrl);
+                employees.Add(currentEmployee);
+            }
+
+            return employees;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,36 +4,9 @@ namespace CatWorx.BadgeMaker
 {
     class Program
     {
-        static List<Employee> GetEmployees()
-        {
-            List<Employee> employees = new List<Employee>();
-            // Collect user values until the value is an empty string
-            while (true)
-            {
-                Console.WriteLine("Please enter a  first name (leave empty to exit): ");
-                string firstName = Console.ReadLine() ?? "";
-                // Break if the user hits ENTER without typing a name
-                if (firstName == "")
-                {
-                    break;
-                }
-
-                Console.Write("Enter a last name: ");
-                string lastName = Console.ReadLine() ?? "";
-                Console.Write("Enter ID: ");
-                int id = Int32.Parse(Console.ReadLine() ?? "");
-                Console.Write("Enter a photo URL: ");
-                string photoUrl = Console.ReadLine() ?? "";
-                Employee currentEmployee = new Employee(firstName, lastName, id, photoUrl);
-                employees.Add(currentEmployee);
-            }
-
-            return employees;
-        }
-
         async static Task Main(string[] args)
         {
-            List<Employee> employees = GetEmployees();
+            List<Employee> employees = PeopleFetcher.GetEmployees();
             Util.PrintEmployees(employees);
             Util.MakeCSV(employees);
             await Util.MakeBadges(employees);
