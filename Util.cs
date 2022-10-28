7c3d31fdae0e63fef9dadf6b79196a09f941f9cf
Alan Phelps <sora64@macbook-air.myfiosgateway.com>
2022-10-28 15:51:01 -0400

    initial commit

---
+++ Util.cs
@@ -0,0 +1,33 @@
+namespace CatWorx.BadgeMaker
+{
+    class Util
+    {
+        public static void PrintEmployees(List<Employee> employees)
+        {
+            for (int i = 0; i < employees.Count; i++)
+            {
+                string template = "{0, -10}\t{1, -20}\t{2}";
+                Console.WriteLine(String.Format(template, employees[i].GetId(), employees[i].GetFullName(), employees[i].GetPhotoUrl()));
+            }
+        }
+
+        public static void MakeCSV(List<Employee> employees)
+        {
+            if (!Directory.Exists("data"))
+            {
+                Directory.CreateDirectory("data");
+            }
+
+            using (StreamWriter file = new StreamWriter("data/employees.csv"))
+            {
+                file.WriteLine("ID, Name, PhotUrl");
+
+                for (int i = 0; i < employees.Count; i++)
+                {
+                    string template = "{0}, {1}, {2}";
+                    file.WriteLine(String.Format(template, employees[i].GetId(), employees[i].GetFullName(), employees[i].GetPhotoUrl()));
+                }
+            }
+        }
+    }
+}
