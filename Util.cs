8b5b71f28ddf7f16ec62c63759b0c8896a5b80da
Alan Phelps <sora64@macbook-air.myfiosgateway.com>
2022-10-28 16:48:50 -0400

    employee image now added to badge

--- Util.cs
+++ Util.cs
@@ -1,3 +1,5 @@
+using SkiaSharp;
+
 namespace CatWorx.BadgeMaker
 {
     class Util
@@ -24,8 +26,38 @@ namespace CatWorx.BadgeMaker
 
                 for (int i = 0; i < employees.Count; i++)
                 {
-                    string template = "{0}, {1}, {2}";
+                    string template = "{0},{1},{2}";
                     file.WriteLine(String.Format(template, employees[i].GetId(), employees[i].GetFullName(), employees[i].GetPhotoUrl()));
+                }
+            }
+        }
+
+        async public static Task MakeBadges(List<Employee> employees)
+        {
+            int BADGE_WIDTH = 669;
+            int BADGE_HEIGHT = 1044;
+
+            int PHOTO_LEFT_X = 184;
+            int PHOTO_TOP_Y = 215;
+            int PHOTO_RIGHT_X = 486;
+            int PHOTO_BOTTOM_Y = 517;
+
+            using (HttpClient client = new HttpClient())
+            {
+                for (int i = 0; i < employees.Count; i++)
+                {
+                    SKImage photo = SKImage.FromEncodedData(await client.GetStreamAsync(employees[i].GetPhotoUrl()));
+                    SKImage background = SKImage.FromEncodedData(File.OpenRead("badge.png"));
+
+                    SKBitmap badge = new SKBitmap(BADGE_WIDTH, BADGE_HEIGHT);
+                    SKCanvas canvas = new SKCanvas(badge);
+
+                    canvas.DrawImage(background, new SKRect(0, 0, BADGE_WIDTH, BADGE_HEIGHT));
+                    canvas.DrawImage(photo, new SKRect(PHOTO_LEFT_X, PHOTO_TOP_Y, PHOTO_RIGHT_X, PHOTO_BOTTOM_Y));
+
+                    SKImage finalImage = SKImage.FromBitmap(badge);
+                    SKData data = finalImage.Encode();
+                    data.SaveTo(File.OpenWrite("data/employeeBadge.png"));
                 }
             }
         }
